94c95baa322bb37ad2c48e77d1ffcc27dea87d24
NourelhoudaAkbi <nourelhoudaakbi@gmail.com>
2024-09-23 20:32:55 +0100

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Nour el houda Akbi on 23/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
